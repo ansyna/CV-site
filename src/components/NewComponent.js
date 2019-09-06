da9da5a465714b6121fa5d7a00e1794b42cb39ae
Hanna Golikova <hgolikov@afilias.info>
2019-09-06 16:04:55 +0100

    Initial commit

---
+++ src/components/NewComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NewComponent extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>My component</h1>
+      </div>
+    );
+  }
+}
